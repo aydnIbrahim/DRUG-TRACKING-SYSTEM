5f55e68410128a257d3ae876a59c7b2fc67f0f3c
İbrahim Aydın <aydnibrahim22@gmail.com>
2024-01-24 18:04:23 +0300

    DRUG TRACKING SYSTEM

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,5 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         new LOGIN_PAGE();
-        //new ACCOUNT_PAGE();
-        //new MAIN_PAGE();
-
     }
 }
